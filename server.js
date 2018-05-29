6b4b23b6b601876536247bc130d81158e4fbac55
Zoealexandra <zoebarron@Zoes-MacBook-Pro.local>
2018-05-29 13:02:13 +1200

    added register route

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const sqlite3 = require('sqlite3').verbose()
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 const PORT = process.env.PORT || 3128
 
 const server = express()
@@ -15,3 +17,31 @@ server.listen(PORT, () =>
 // eslint-disable-next-line no-console
   console.log(`App running on localhost:${PORT}`)
 )
+
+
+server.post('/register', (req,res) => {
+  //ensure no empty fields
+  if(isEmpty(req.body.name) || isEmpty(req.body.email) || isEmpty(req.body.company_name) || isEmpty(req.body.password) ) {
+    return res.json({
+      'status': false,
+      'message': 'All fields are required'
+    })
+  }
+  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+    let db = new sqlite3.Database('./database/invoiceingApp.db')
+    let sql = `INSERT INTO users(name,email,company_name,password) VALUES('${
+      req.body.name
+    }','${req.body.email}','${req.body.company_name}','${hash}')`
+    db.run(sql, err => {
+      if (err) {
+        throw err
+      } else {
+        return res.json({
+          status: true,
+          message: 'User Created'
+        })
+      }
+    })
+    db.close
+  })
+})
